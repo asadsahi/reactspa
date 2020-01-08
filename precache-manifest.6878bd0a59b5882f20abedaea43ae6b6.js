e9dafb8c8cd46bc43c926a27fb5c37e9548955e9
asadsahi <asadsahi@users.noreply.github.com>
2020-01-08 17:55:01 +0000

    Automated deployment: Wed Jan  8 17:55:01 UTC 2020 72c6a0f8ff2852041ef745f456dd65fa169461e5

---
+++ precache-manifest.6878bd0a59b5882f20abedaea43ae6b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11cfeea4392ac97b757c7c7712469a6",
+    "url": "/reactspa/index.html"
+  },
+  {
+    "revision": "ba63dada4703b1f46851",
+    "url": "/reactspa/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "b73bcb1e6099e95abc12",
+    "url": "/reactspa/static/css/main.553fc9a1.chunk.css"
+  },
+  {
+    "revision": "ba63dada4703b1f46851",
+    "url": "/reactspa/static/js/2.333a9a6b.chunk.js"
+  },
+  {
+    "revision": "97dd8b6bd7f02ee5ebd75e39cc26400d",
+    "url": "/reactspa/static/js/2.333a9a6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b73bcb1e6099e95abc12",
+    "url": "/reactspa/static/js/main.be79a5bc.chunk.js"
+  },
+  {
+    "revision": "116ec8a251e934d38751",
+    "url": "/reactspa/static/js/runtime-main.1b56aac6.js"
+  }
+]);
