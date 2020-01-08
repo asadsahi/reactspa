cf03030079fee9f2a14723ff485dc18536341e40
Asad Sahi <asadazmat@gmail.com>
2020-01-08 21:46:17 +0000

    icons

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
+
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+import { Examples } from './components/examples';
 
-import './custom.css'
+import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
+        <Route exact path="/" component={Home} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+        <AuthorizeRoute path="/examples" component={Examples} />
       </Layout>
     );
   }
---
+++ src/components/examples/Examples.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Examples extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Examples</h1>
+        <p>Coming soon...</p>
+      </div>
+    );
+  }
+}
